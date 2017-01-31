1970561cda59b42c0c45ad394efbfcfdd1db89e7
jh0rr <jhorr@wccnet.edu>
2017-01-31 17:23:19 -0500

    Added TALONSRXDrive class

---
+++ src/TalonSRXDrive.cpp
@@ -0,0 +1,18 @@
+/*
+ * TalonSRXDrive.cpp
+ *
+ *  Created on: Jan 31, 2017
+ *      Author: Jackson
+ */
+
+#include <TalonSRXDrive.h>
+
+TalonSRXDrive::TalonSRXDrive() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TalonSRXDrive::~TalonSRXDrive() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TalonSRXDrive.h
@@ -0,0 +1,17 @@
+/*
+ * TalonSRXDrive.h
+ *
+ *  Created on: Jan 31, 2017
+ *      Author: Jackson
+ */
+
+#ifndef SRC_TALONSRXDRIVE_H_
+#define SRC_TALONSRXDRIVE_H_
+
+class TalonSRXDrive {
+public:
+	TalonSRXDrive();
+	virtual ~TalonSRXDrive();
+};
+
+#endif /* SRC_TALONSRXDRIVE_H_ */
